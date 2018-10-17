6ea45c85a1cb8830e0f74ee63ef86cd85bb13d3d
unknown <1005682567@qq.com>
2018-10-17 12:18:43 +0800

    0.1

---
+++ ChangeWallPaper/Form1.Designer.cs
@@ -0,0 +1,149 @@
+﻿namespace ChangeWallPaper
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.textBox_seconds = new System.Windows.Forms.TextBox();
+            this.label_seconds = new System.Windows.Forms.Label();
+            this.button_refresh = new System.Windows.Forms.Button();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("宋体", 22.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.Location = new System.Drawing.Point(288, 64);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(283, 38);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "壁纸循环播放器";
+            // 
+            // button1
+            // 
+            this.button1.AutoSize = true;
+            this.button1.Font = new System.Drawing.Font("宋体", 22.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button1.Location = new System.Drawing.Point(295, 220);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(255, 48);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "选择图片路径";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("宋体", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.textBox1.Location = new System.Drawing.Point(109, 151);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(641, 38);
+            this.textBox1.TabIndex = 2;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick_1);
+            // 
+            // textBox_seconds
+            // 
+            this.textBox_seconds.Font = new System.Drawing.Font("宋体", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.textBox_seconds.Location = new System.Drawing.Point(239, 353);
+            this.textBox_seconds.Name = "textBox_seconds";
+            this.textBox_seconds.Size = new System.Drawing.Size(100, 34);
+            this.textBox_seconds.TabIndex = 3;
+            // 
+            // label_seconds
+            // 
+            this.label_seconds.AutoSize = true;
+            this.label_seconds.Font = new System.Drawing.Font("宋体", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label_seconds.Location = new System.Drawing.Point(345, 363);
+            this.label_seconds.Name = "label_seconds";
+            this.label_seconds.Size = new System.Drawing.Size(82, 24);
+            this.label_seconds.TabIndex = 4;
+            this.label_seconds.Text = "秒更新";
+            // 
+            // button_refresh
+            // 
+            this.button_refresh.AutoSize = true;
+            this.button_refresh.Font = new System.Drawing.Font("宋体", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button_refresh.Location = new System.Drawing.Point(446, 353);
+            this.button_refresh.Name = "button_refresh";
+            this.button_refresh.Size = new System.Drawing.Size(104, 34);
+            this.button_refresh.TabIndex = 5;
+            this.button_refresh.Text = "更新";
+            this.button_refresh.UseVisualStyleBackColor = true;
+            this.button_refresh.Click += new System.EventHandler(this.button_refresh_Click);
+            // 
+            // timer2
+            // 
+            this.timer2.Enabled = true;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button_refresh);
+            this.Controls.Add(this.label_seconds);
+            this.Controls.Add(this.textBox_seconds);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "ChangeWallPaper";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.TextBox textBox_seconds;
+        private System.Windows.Forms.Label label_seconds;
+        private System.Windows.Forms.Button button_refresh;
+        private System.Windows.Forms.Timer timer2;
+    }
+}
+
---
+++ ChangeWallPaper/Form1.cs
@@ -0,0 +1,129 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ChangeWallPaper
+{
+    public partial class Form1 : Form
+    {
+        public int refresh_seconds = 3*1000;
+        public string filePath = "C:\\Users\\Amazing\\Desktop\\20171127_IMG_0003.JPG";
+        public string[] images;
+        public int cnt = 0;
+        public bool working = false;
+
+        [DllImport("user32.dll", EntryPoint = "SystemParametersInfo")]
+        public static extern int SystemParametersInfo(
+                int uAction,
+                int uParam,
+                string lpvParam,
+                int fuWinIni
+                );
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// 判断字符串是否是数字
+        /// </summary>
+        public static bool IsNumber(string s)
+        {
+            if (string.IsNullOrWhiteSpace(s)) return false;
+            const string pattern = "^[0-9]*$";
+            Regex rx = new Regex(pattern);
+            return rx.IsMatch(s);
+        }
+
+        public void update_location()
+        {
+            label1.Left = this.Width / 2 - label1.Width / 2;
+            textBox1.Left = this.Width / 2 - textBox1.Width / 2;
+            button1.Left = this.Width / 2 - button1.Width / 2;
+            label_seconds.Left = this.Width / 2 - label_seconds.Width / 2;
+            textBox_seconds.Left = label_seconds.Left- textBox_seconds.Width;
+            button_refresh.Left = label_seconds.Left + label_seconds.Width;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                textBox1.Text = folderBrowserDialog1.SelectedPath;
+            }
+        }
+
+        private void timer1_Tick_1(object sender, EventArgs e)
+        {
+            update_location();
+        }
+
+        //利用系统的用户接口设置壁纸
+        
+        public static void setWallpaperApi(string strSavePath)
+        {
+           SystemParametersInfo(20, 1, strSavePath, 1);
+        }
+
+        private void button_refresh_Click(object sender, EventArgs e)
+        {
+            refresh_seconds = 3;
+            if (IsNumber(textBox_seconds.Text))
+            {
+                refresh_seconds = Convert.ToInt32(textBox_seconds.Text);
+            }
+            else
+            {
+                textBox_seconds.Text = "3";
+            }
+
+            if(textBox1.Text!="")
+            {
+                filePath = textBox1.Text;
+            }
+            else
+            {
+                filePath= "C:\\Users\\Amazing\\Desktop\\20171127_IMG_0003.JPG";
+            }
+
+            timer2.Interval = refresh_seconds * 1000;
+
+            get_images_path();
+
+            working = true;
+        }
+
+        public void get_images_path()
+        {
+            var a = Directory.GetFiles(filePath, "*.*", SearchOption.AllDirectories).Where(s => s.EndsWith(".bmp") || s.EndsWith(".jpg"));
+            images = a.ToArray();
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            if(working)
+            {
+                setWallpaperApi(images[cnt++]);
+                cnt %= images.Length;
+            }
+            else
+            {
+                setWallpaperApi(filePath);
+            }
+        }
+    }
+}
